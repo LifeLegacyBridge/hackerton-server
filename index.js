80990b7a71fe0c9730baaa1ef3d01c54fc6cc23f
ian <hope030327@naver.com>
2025-01-18 22:38:32 +0900

    머지 전 커밋

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import db from './src/database/index.js';
 import { authRouter } from './src/route/auth.route.js';
 import { userRouter } from './src/route/user.router.js';
 import { chatgptRouter } from './src/route/chatgpt.route.js';
+import { postRouter } from './src/route/post.router.js';
 
 const app = express();
 const PORT = 3000;
@@ -29,6 +30,7 @@ app.get('/', (req, res) => {
 app.use('/auth',authRouter);
 app.use('/user',userRouter);
 app.use('/chatgpt',chatgptRouter);
+app.use('/post', postRouter);
 
 app.listen(PORT, () => {
     console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
